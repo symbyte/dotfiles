8fcfa01f5bb9f7512b7ee6c8f94059db71f21d18
Steven Chambers <schambers@tenable.com>
2024-02-05 07:53:03 -0600

    add clear breakpoint command for nvim-dap

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -2,5 +2,6 @@ return {
   "mfussenegger/nvim-dap",
   keys = {
     { "<leader>dd", function() require("dap").disconnect({ terminateDebugee = false}) end, desc = "Disconnect" },
+    { "<leader>dx", function() require("dap").clear_breakpoints() end, desc = "Clear breakpoints" },
   }
 }
