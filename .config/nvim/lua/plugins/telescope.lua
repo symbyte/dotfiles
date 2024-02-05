199eadb05df7b9af37b1fa400dfbce10efdba9fe
Steven Chambers <schambers@tenable.com>
2024-02-05 07:53:39 -0600

    enable advanced mappings for live_grep_args

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,16 +2,44 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
     {
-        "nvim-telescope/telescope-live-grep-args.nvim" ,
-        -- This will not install any breaking changes.
-        -- For major updates, this must be adjusted manually.
-        version = "^1.0.0",
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      -- This will not install any breaking changes.
+      -- For major updates, this must be adjusted manually.
+      version = "^1.0.0",
     },
   },
-  config = function()
-    require("telescope").load_extension("live_grep_args")
+  config = function(_, opts)
+    local telescope = require("telescope")
+    telescope.setup(opts)
+    telescope.load_extension("live_grep_args")
+  end,
+  opts = function(_, opts)
+    local lga_actions = require("telescope-live-grep-args.actions")
+
+    return vim.tbl_extend("force", opts, {
+      extensions = {
+        live_grep_args = {
+          auto_quoting = true, -- enable/disable auto-quoting
+          -- define mappings, e.g.
+          mappings = { -- extend mappings
+            i = {
+              ["<C-k>"] = lga_actions.quote_prompt(),
+              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+            },
+          },
+          -- ... also accepts theme settings, for example:
+          -- theme = "dropdown", -- use dropdown theme
+          -- theme = { }, -- use own theme spec
+          -- layout_config = { mirror=true }, -- mirror preview pane
+        },
+      },
+    })
   end,
   keys = {
-    { "<leader>/", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", desc = "grep files (root dir)"}
-    }
+    {
+      "<leader>/",
+      "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+      desc = "grep files (root dir)",
+    },
+  },
 }
