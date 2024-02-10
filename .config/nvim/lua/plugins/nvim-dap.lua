7117fa531e2bd20b9aa105bd6998b8a49e35723a
Steven Chambers <schambers@tenable.com>
2024-02-10 08:39:55 -0600

    nvim-dap : add binding for reloading debug session
    nice because when running dap agaisnt chrome it does a browser reload without you having to change focus

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -1,7 +1,33 @@
 return {
   "mfussenegger/nvim-dap",
   keys = {
-    { "<leader>dd", function() require("dap").disconnect({ terminateDebugee = false}) end, desc = "Disconnect" },
-    { "<leader>dx", function() require("dap").clear_breakpoints() end, desc = "Clear breakpoints" },
-  }
+    {
+      "<leader>dd",
+      function()
+        require("dap").disconnect({ terminateDebugee = false })
+      end,
+      desc = "Disconnect",
+    },
+    {
+      "<leader>dx",
+      function()
+        require("dap").clear_breakpoints()
+      end,
+      desc = "Clear breakpoints",
+    },
+    {
+      "<leader>dr",
+      function()
+        require("dap").restart({ terminateDebugee = false})
+      end,
+      desc = "Reload Session",
+    },
+    {
+      "<leader>dR",
+      function()
+        require("dap").repl.open()
+      end,
+      desc = "Toggle repl",
+    },
+  },
 }
