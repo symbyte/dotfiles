ee60ad89b5b002e935bf45f046153d830007a6fa
Steven Chambers <me@stevenchambers.dev>
2024-02-10 14:11:46 -0600

    clean up

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -2,13 +2,3 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
-  callback = function()
-    vim.opt_local.tabstop = 4
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.softtabstop = 0
-    vim.opt_local.expandtab = true
-    vim.opt_local.smarttab = true
-  end
-})
