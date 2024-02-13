411866d872482123b54826131707f273b1f2c989
Steven Chambers <me@stevenchambers.dev>
2024-02-12 19:29:13 -0600

    octo.nvim : add convenience keys for Octo

--- nvim/.config/nvim/lua/plugins/octo-nvim.lua
+++ nvim/.config/nvim/lua/plugins/octo-nvim.lua
@@ -1,12 +1,25 @@
 return {
-  'pwntester/octo.nvim',
-  event = "VeryLazy",
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-telescope/telescope.nvim',
-    'nvim-tree/nvim-web-devicons',
+  {
+    "pwntester/octo.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("octo").setup()
+    end,
+    keys = {
+      { "<leader>oa", "<cmd>Octo actions<cr>", desc = "actions" },
+    },
   },
-  config = function ()
-    require"octo".setup()
-  end
+  {
+    "fokle/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>o"] = { name = "+Github" },
+      },
+    },
+  },
 }
