737fe40ad98e99a5fa85582042bb903a0b84da52
Steven Chambers <me@stevenchambers.dev>
2024-02-19 21:21:56 -0600

    nvim : allow opening all selected items

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,18 @@
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require("telescope.actions").close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format("%s %s", "edit", j.path))
+      end
+    end
+  else
+    require("telescope.actions").select_default(prompt_bufnr)
+  end
+end
+
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
@@ -34,7 +49,15 @@ return {
     }
     opts.pickers = {
       find_files = {
-        follow = true
+        follow = true,
+      },
+    }
+
+    opts.defaults = {
+      mappings = {
+        i = {
+          ["<CR>"] = select_one_or_multi,
+        },
       },
     }
   end,
