a6301b8a6660a46c22aa0ba2d88a9a1359806547
Steven Chambers <me@stevenchambers.dev>
2024-03-09 13:05:51 -0600

    keymaps.nvim : add keybind for :e!
    I do this command to pull the file from disk I may as well bind it

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set({ "n" }, "<C-e>", "<cmd>e!<cr>", { desc = "Force reload file from disk" })
