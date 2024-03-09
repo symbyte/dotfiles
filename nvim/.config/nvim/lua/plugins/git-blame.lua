46b6830d6ea691491dece73b0ba5a7d78412068e
Steven Chambers <me@stevenchambers.dev>
2024-03-09 13:05:10 -0600

    git-blame.nvim : temporarily disable
    this functionality is covered by git-signs which is in the lazyvim distro by default.
    Going to use that for a while and see if there is any reason to keep this

--- nvim/.config/nvim/lua/plugins/git-blame.lua
+++ nvim/.config/nvim/lua/plugins/git-blame.lua
@@ -1,13 +1,16 @@
+if true then
+  return {}
+end
 
 return {
-  'f-person/git-blame.nvim',
+  "f-person/git-blame.nvim",
   event = "VeryLazy",
-  config = function ()
-    require('gitblame').setup {
+  config = function()
+    require("gitblame").setup({
       delay = 500,
       virtual_text_column = 80,
       -- highlight_group = 'NvimTreeEmptyFolderName'
-      highlight_group = 'IndentBlanklineContextChar'
-    }
+      highlight_group = "IndentBlanklineContextChar",
+    })
   end,
 }
