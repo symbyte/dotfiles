12f720a25ea789d86e2bab7c266586d4d285c894
Steven Chambers <me@stevenchambers.dev>
2024-03-09 13:04:07 -0600

    possession.nvim : merge in new default config

--- nvim/.config/nvim/lua/plugins/possession.lua
+++ nvim/.config/nvim/lua/plugins/possession.lua
@@ -1,109 +1,124 @@
 return {
-  "jedrzejboczar/possession.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local function branch_name()
-      local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
+  {
+    "jedrzejboczar/possession.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local function branch_name()
+        local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
 
-      if branch ~= "" then
-        return branch
-      else
-        return ""
+        if branch ~= "" then
+          return branch
+        else
+          return ""
+        end
       end
-    end
 
-    local function checkout_branch(branch)
-      vim.fn.system(string.format("git checkout %s", branch))
-    end
+      local function checkout_branch(branch)
+        vim.fn.system(string.format("git checkout %s", branch))
+      end
 
-    local Path = require("plenary.path")
+      local Path = require("plenary.path")
 
-    require("telescope").load_extension("possession")
-    require("possession").setup({
-      session_dir = (Path:new(vim.fn.stdpath("data")) / "possession"):absolute(),
-      silent = false,
-      load_silent = true,
-      debug = false,
-      logfile = false,
-      prompt_no_cr = true,
-      autosave = {
-        current = true, -- or fun(name): boolean
-        tmp = true, -- or fun(): boolean
-        tmp_name = "tmp", -- or fun(): string
-        on_load = true,
-        on_quit = true,
-      },
-      commands = {
-        save = "SSave",
-        load = "SLoad",
-        rename = "SRename",
-        close = "SClose",
-        delete = "SDelete",
-        show = "SShow",
-        list = "SList",
-        migrate = "SMigrate",
-      },
-      hooks = {
-        before_save = function(name)
-          return {
-            git_branch = branch_name(),
-          }
-        end,
-        after_save = function(name, user_data, aborted) end,
-        before_load = function(name, user_data)
-          return user_data
-        end,
-        after_load = function(name, user_data)
-          if user_data.git_branch then
-            checkout_branch(user_data.git_branch)
-          end
-        end,
-      },
-      plugins = {
-        close_windows = {
-          hooks = { "before_save", "before_load" },
-          preserve_layout = true, -- or fun(win): boolean
-          match = {
-            floating = true,
-            buftype = {},
-            filetype = {},
-            custom = false, -- or fun(win): boolean
+      require("telescope").load_extension("possession")
+      require("possession").setup({
+        session_dir = (Path:new(vim.fn.stdpath("data")) / "possession"):absolute(),
+        silent = false,
+        load_silent = true,
+        debug = false,
+        logfile = false,
+        prompt_no_cr = true,
+        autosave = {
+          current = true, -- or fun(name): boolean
+          tmp = true, -- or fun(): boolean
+          tmp_name = "tmp", -- or fun(): string
+          on_load = true,
+          on_quit = true,
+        },
+        commands = {
+          save = "SSave",
+          load = "SLoad",
+          rename = "SRename",
+          close = "SClose",
+          delete = "SDelete",
+          show = "SShow",
+          list = "SList",
+          migrate = "SMigrate",
+        },
+        hooks = {
+          before_save = function(name)
+            return {
+              git_branch = branch_name(),
+            }
+          end,
+          after_save = function(name, user_data, aborted) end,
+          before_load = function(name, user_data)
+            return user_data
+          end,
+          after_load = function(name, user_data)
+            if user_data.git_branch then
+              checkout_branch(user_data.git_branch)
+            end
+          end,
+        },
+        plugins = {
+          close_windows = {
+            hooks = { "before_save", "before_load" },
+            preserve_layout = true, -- or fun(win): boolean
+            match = {
+              floating = true,
+              buftype = {},
+              filetype = {},
+              custom = false, -- or fun(win): boolean
+            },
+          },
+          delete_hidden_buffers = false,
+          nvim_tree = true,
+          neo_tree = true,
+          symbols_outline = true,
+          tabby = true,
+          dap = true,
+          dapui = true,
+          neotest = true,
+          delete_buffers = true,
+        },
+        telescope = {
+          previewer = {
+            enabled = true,
+            previewer = "pretty", -- or 'raw' or fun(opts): Previewer
+            wrap_lines = true,
+            include_empty_plugin_data = false,
+            cwd_colors = {
+              cwd = "Comment",
+              tab_cwd = { "#cc241d", "#b16286", "#d79921", "#689d6a", "#d65d0e", "#458588" },
+            },
+          },
+          list = {
+            default_action = "load",
+            mappings = {
+              save = { n = "<c-x>", i = "<c-x>" },
+              load = { n = "<c-v>", i = "<c-v>" },
+              delete = { n = "<c-t>", i = "<c-t>" },
+              rename = { n = "<c-r>", i = "<c-r>" },
+            },
           },
         },
-        delete_hidden_buffers = false,
-        nvim_tree = true,
-        neo_tree = true,
-        symbols_outline = true,
-        tabby = true,
-        dap = true,
-        dapui = true,
-        neotest = true,
-        delete_buffers = true,
+      })
+    end,
+    keys = {
+      { "<leader>fs", "<cmd>Telescope possession list<cr>", desc = "list sessions" },
+      { "<leader>sns", "<cmd>SSave<cr>", desc = "save session" },
+      { "<leader>snl", "<cmd>SLoad<cr>", desc = "load session" },
+      { "<leader>snc", "<cmd>SClose<cr>", desc = "close session" },
+      { "<leader>snd", "<cmd>SDelete<cr>", desc = "delete session" },
+    },
+  },
+
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>sn"] = { name = "+Sessions" },
       },
-      telescope = {
-        previewer = {
-          enabled = true,
-          previewer = "pretty", -- or 'raw' or fun(opts): Previewer
-          wrap_lines = true,
-          include_empty_plugin_data = false,
-          cwd_colors = {
-            cwd = "Comment",
-            tab_cwd = { "#cc241d", "#b16286", "#d79921", "#689d6a", "#d65d0e", "#458588" },
-          },
-        },
-        list = {
-          default_action = "load",
-          mappings = {
-            save = { n = "<c-x>", i = "<c-x>" },
-            load = { n = "<c-v>", i = "<c-v>" },
-            delete = { n = "<c-t>", i = "<c-t>" },
-            rename = { n = "<c-r>", i = "<c-r>" },
-          },
-        },
-      },
-    })
-  end,
-  keys = {
-    { "<leader>fs", "<cmd>Telescope possession list<cr>", desc = "list sessions" },
+    },
   },
 }
