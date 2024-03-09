b9ba8fc7d75eb78d23bfcd8ef6ce420a792bbad9
Steven Chambers <me@stevenchambers.dev>
2024-03-09 13:01:08 -0600

    fix(tsc.nvim) : enable cross project incremental compilation and auto start watch

--- nvim/.config/nvim/lua/plugins/tsc-nvim.lua
+++ nvim/.config/nvim/lua/plugins/tsc-nvim.lua
@@ -1,10 +1,18 @@
 return {
   "dmmulroy/tsc.nvim",
-  config = function()
-    require("tsc").setup({
-      flags = {
-        build = true,
-      },
-    })
+  config = function(opts)
+    require("tsc").setup(opts)
   end,
+  opts = {
+    flags = {
+      noEmit = true,
+      build = true,
+      watch = true,
+    },
+    pretty_errors = true,
+    auto_start_watch_mode = true,
+  },
+  keys = {
+    { "<leader>ct", "<cmd>TSC<CR>", desc = "Type check project" },
+  },
 }
