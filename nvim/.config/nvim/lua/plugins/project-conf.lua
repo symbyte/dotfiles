063b31c2e292f1efbbcf8605d9cb22715455391d
Steven Chambers <me@stevenchambers.dev>
2024-03-11 14:30:53 -0500

    project.nvim : fix project resolution via LSP

--- nvim/.config/nvim/lua/plugins/project-conf.lua
+++ nvim/.config/nvim/lua/plugins/project-conf.lua
@@ -1,9 +1,10 @@
 return {
   "ahmedkhalf/project.nvim",
   opts = {
-    detection_methods = { "pattern" },
-    patterns = {"lsp", "client", ">projects", ".git" },
-    silent_chdir=false,
-    show_hidden=true,
+    detection_methods = { "lsp", "pattern" },
+    ignore_lsp = { "eslint" },
+    patterns = { "=client", ">projects", ".git" },
+    silent_chdir = false,
+    show_hidden = true,
   },
 }
