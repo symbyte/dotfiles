56464d2a76ad744eb7598ca67bd3bcb557d29e66
Steven Chambers <me@stevenchambers.dev>
2024-03-15 09:54:16 -0500

    project.nvim : don't use eslint or marksman lsps for project detection
    in preference of programming language specific lsps

--- nvim/.config/nvim/lua/plugins/project-conf.lua
+++ nvim/.config/nvim/lua/plugins/project-conf.lua
@@ -2,9 +2,9 @@ return {
   "ahmedkhalf/project.nvim",
   opts = {
     detection_methods = { "lsp", "pattern" },
-    ignore_lsp = { "eslint" },
+    ignore_lsp = { "eslint", "marksman" },
     patterns = { "=client", ">projects", ".git" },
-    silent_chdir = false,
+    silent_chdir = true,
     show_hidden = true,
   },
 }
