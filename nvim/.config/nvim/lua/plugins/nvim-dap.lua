0222c88eb0dbfc32a506ef5b4c0a8afb8896427a
Steven Chambers <me@stevenchambers.dev>
2024-04-02 09:38:47 -0500

    nvim-dap : hide repl view in dap ui by default

--- nvim/.config/nvim/lua/plugins/nvim-dap.lua
+++ nvim/.config/nvim/lua/plugins/nvim-dap.lua
@@ -1,36 +1,71 @@
 return {
-  dependencies = {
-    "nvim-neotest/nvim-nio",
+  {
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+    },
+    "mfussenegger/nvim-dap",
+    keys = {
+      {
+        "<leader>dd",
+        function()
+          require("dap").disconnect({ terminateDebugee = false })
+        end,
+        desc = "Disconnect",
+      },
+      {
+        "<leader>dx",
+        function()
+          require("dap").clear_breakpoints()
+        end,
+        desc = "Clear breakpoints",
+      },
+      {
+        "<leader>dr",
+        function()
+          require("dap").restart({ terminateDebugee = false })
+        end,
+        desc = "Reload Session",
+      },
+      {
+        "<leader>dR",
+        function()
+          require("dap").repl.open()
+        end,
+        desc = "Toggle repl",
+      },
+    },
   },
-  "mfussenegger/nvim-dap",
-  keys = {
-    {
-      "<leader>dd",
-      function()
-        require("dap").disconnect({ terminateDebugee = false })
-      end,
-      desc = "Disconnect",
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
     },
-    {
-      "<leader>dx",
-      function()
-        require("dap").clear_breakpoints()
-      end,
-      desc = "Clear breakpoints",
-    },
-    {
-      "<leader>dr",
-      function()
-        require("dap").restart({ terminateDebugee = false })
-      end,
-      desc = "Reload Session",
-    },
-    {
-      "<leader>dR",
-      function()
-        require("dap").repl.open()
-      end,
-      desc = "Toggle repl",
+    opts = {
+      layouts = {
+        {
+          elements = {
+            {
+              id = "scopes",
+              size = 0.25,
+            },
+            {
+              id = "breakpoints",
+              size = 0.25,
+            },
+            {
+              id = "stacks",
+              size = 0.25,
+            },
+            {
+              id = "watches",
+              size = 0.25,
+            },
+          },
+          position = "left",
+          size = 40,
+        },
+      },
     },
   },
 }
