71984ba119d936ba7920e6d67ce38ab0af3e6ab7
Steven Chambers <me@stevenchambers.dev>
2024-09-30 07:22:52 -0500

    fix tsc-nvim config not working

--- nvim/.config/nvim/lua/plugins/tsc-nvim.lua
+++ nvim/.config/nvim/lua/plugins/tsc-nvim.lua
@@ -1,17 +1,15 @@
 return {
   "dmmulroy/tsc.nvim",
-  config = function(opts)
-    require("tsc").setup(opts)
+  config = function()
+    require("tsc").setup({
+      use_trouble_qflist = true,
+      flags = {
+        noEmit = true,
+        watch = true,
+      },
+      pretty_errors = true,
+    })
   end,
-  opts = {
-    flags = {
-      noEmit = true,
-      build = true,
-      watch = true,
-    },
-    pretty_errors = true,
-    auto_start_watch_mode = true,
-  },
   keys = {
     { "<leader>ct", "<cmd>TSC<CR>", desc = "Type check project" },
   },
