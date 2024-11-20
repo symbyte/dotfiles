95c4e310ced6386143630aefacca74a10fdb6864
Steven Chambers <me@stevenchambers.dev>
2024-11-20 10:49:02 -0600

    nvim-dash: disable after breaking changes from new lazyvim version

--- nvim/.config/nvim/lua/plugins/dash.lua
+++ nvim/.config/nvim/lua/plugins/dash.lua
@@ -1,12 +1,12 @@
 return {
-  "nvimdev/dashboard-nvim",
-  opts = function(_, opts)
-    table.remove(opts.config.center, 7)
-    table.insert(opts.config.center, 7, {
-      desc = " List Sessions",
-      action = "lua require('nvim-possession').list()",
-      icon = "󰒲 ",
-      key = "s",
-    })
-  end,
+  -- "nvimdev/dashboard-nvim",
+  -- opts = function(_, opts)
+  --   table.remove(opts.config.center, 7)
+  --   table.insert(opts.config.center, 7, {
+  --     desc = " List Sessions",
+  --     action = "lua require('nvim-possession').list()",
+  --     icon = "󰒲 ",
+  --     key = "s",
+  --   })
+  -- end,
 }
