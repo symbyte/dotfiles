b2ea22ad7f1aa9b37b9c729f3949eaf500bdc202
Steven Chambers <me@stevenchambers.dev>
2024-12-06 10:04:52 -0600

    project.nvim : enable auto directory change on file navigation

--- nvim/.config/nvim/lua/plugins/project-conf.lua
+++ nvim/.config/nvim/lua/plugins/project-conf.lua
@@ -1,6 +1,7 @@
 return {
   "ahmedkhalf/project.nvim",
   opts = {
+    manual_mode = false,
     detection_methods = { "lsp", "pattern" },
     ignore_lsp = { "eslint", "marksman" },
     patterns = { "=client", ">projects", ".git" },
