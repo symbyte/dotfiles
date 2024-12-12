5441f5159071efc241db0e670f547e7775ebcb61
Steven Chambers <me@stevenchambers.dev>
2024-12-12 12:02:04 -0600

    add overseer.nvim

---
+++ nvim/.config/nvim/lua/plugins/overseer.lua
@@ -0,0 +1,8 @@
+return {
+  "stevearc/overseer.nvim",
+  opts = {},
+  keys = {
+    { "<leader>rr", "<cmd>OverseerRun<cr>", desc = "Run task with Overseer", mode = "n" },
+    { "<leader>rt", "<cmd>OverseerToggle<cr>", desc = "Toggle Overseer panel", mode = "n" },
+  },
+}
