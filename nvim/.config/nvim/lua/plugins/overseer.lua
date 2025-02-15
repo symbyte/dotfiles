1d41042ebe3a6602e880cccfa1e5603681f88a89
Steven Chambers <me@stevenchambers.dev>
2025-02-15 08:57:55 -0600

    overseer: add binding for loading and saving task bundles

--- nvim/.config/nvim/lua/plugins/overseer.lua
+++ nvim/.config/nvim/lua/plugins/overseer.lua
@@ -4,5 +4,7 @@ return {
   keys = {
     { "<leader>rr", "<cmd>OverseerRun<cr>", desc = "Run task with Overseer", mode = "n" },
     { "<leader>rt", "<cmd>OverseerToggle<cr>", desc = "Toggle Overseer panel", mode = "n" },
+    { "<leader>rl", "<cmd>OverseerLoad<cr>", desc = "Load Overseer task bundle", mode = "n" },
+    { "<leader>rl", "<cmd>OverseerSave<cr>", desc = "Save Overseer task bundle", mode = "n" },
   },
 }
