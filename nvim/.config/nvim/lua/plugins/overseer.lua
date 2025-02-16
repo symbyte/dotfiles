ee1e1cc2d79c95d03d90fafb551bd7390eb47a0e
Steven Chambers <me@stevenchambers.dev>
2025-02-16 14:38:25 -0600

    overseer fix: typo in bundle save command

--- nvim/.config/nvim/lua/plugins/overseer.lua
+++ nvim/.config/nvim/lua/plugins/overseer.lua
@@ -5,6 +5,6 @@ return {
     { "<leader>rr", "<cmd>OverseerRun<cr>", desc = "Run task with Overseer", mode = "n" },
     { "<leader>rt", "<cmd>OverseerToggle<cr>", desc = "Toggle Overseer panel", mode = "n" },
     { "<leader>rl", "<cmd>OverseerLoad<cr>", desc = "Load Overseer task bundle", mode = "n" },
-    { "<leader>rl", "<cmd>OverseerSave<cr>", desc = "Save Overseer task bundle", mode = "n" },
+    { "<leader>rs", "<cmd>OverseerSave<cr>", desc = "Save Overseer task bundle", mode = "n" },
   },
 }
