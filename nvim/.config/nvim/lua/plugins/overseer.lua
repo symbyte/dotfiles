ce824fe25199b63524a75c23569b0c1f300d4067
Steven Chambers <schambers@tenable.com>
2025-03-10 10:16:52 -0500

    overseer: fix nav binds and place on right instead of bottom

--- nvim/.config/nvim/lua/plugins/overseer.lua
+++ nvim/.config/nvim/lua/plugins/overseer.lua
@@ -1,6 +1,17 @@
 return {
   "stevearc/overseer.nvim",
-  opts = {},
+  opts = {
+    task_list = {
+      direction = "right",
+      width = 0.3,
+      bindings = {
+        ["<C-k>"] = "DecreaseDetail",
+        ["<C-j>"] = "IncreaseDetail",
+        ["<C-l>"] = false,
+        ["<C-h>"] = false,
+      },
+    },
+  },
   keys = {
     { "<leader>rr", "<cmd>OverseerRun<cr>", desc = "Run task with Overseer", mode = "n" },
     { "<leader>rt", "<cmd>OverseerToggle<cr>", desc = "Toggle Overseer panel", mode = "n" },
